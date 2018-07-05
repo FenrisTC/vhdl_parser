bbad0666cdc2138fe466e2ed8768deb66a85522d
Sebastian Schüller <schueller@ti.uni-bonn.de>
2018-07-05 15:42:36 +0200

    reorganise parser tests to clean up visibility

--- src/parser.rs
+++ src/parser.rs
@@ -562,20 +562,20 @@ impl<'srcfile> ParseInfo<'srcfile> {
         return Ok(result);
     }
 
-    pub fn parse_expression(&mut self) -> PResult<Expr> {
+    fn parse_expression(&mut self) -> PResult<Expr> {
         self.parse_expr_with_precedence(0)
     }
 
-    pub fn parse_simple_expression(&mut self) -> PResult<Expr> {
+    fn parse_simple_expression(&mut self) -> PResult<Expr> {
         self.parse_expr_with_precedence(Op::Add.precedence())
     }
 
 
-    pub fn parse_external_name(&mut self) -> PResult<Name> {
+    fn parse_external_name(&mut self) -> PResult<Name> {
         unimplemented!();
     }
 
-    pub fn parse_signature(&mut self) -> PResult<Signature> {
+    fn parse_signature(&mut self) -> PResult<Signature> {
         debug_assert!(self.kind() == LBracket);
 
         let mut signature = Signature::default();
@@ -612,7 +612,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
         Ok(signature)
     }
 
-    pub fn parse_name_prefix_segment(&mut self) -> PResult<NameSegment> {
+    fn parse_name_prefix_segment(&mut self) -> PResult<NameSegment> {
 
         if !self.tok_is_one_of(&[Ident, StringLiteral, CharLiteral]) {
             return self.unexpected_tok();
@@ -646,7 +646,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
         self.tok_is_one_of(&[LtLt, Ident, CharLiteral, StringLiteral])
     }
 
-    pub fn parse_selected_name(&mut self) -> PResult<Name> {
+    fn parse_selected_name(&mut self) -> PResult<Name> {
 
         let mut name = Name::default();
 
@@ -669,7 +669,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
         Ok(name)
     }
 
-    pub fn parse_name(&mut self) -> PResult<Name> {
+    fn parse_name(&mut self) -> PResult<Name> {
         if self.tok_is(LtLt) {
             return self.parse_external_name();
         }
@@ -840,7 +840,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
         Ok(name)
     }
 
-    pub fn parse_resolution_indication(&mut self) -> PResult<ResolutionIndication> {
+    fn parse_resolution_indication(&mut self) -> PResult<ResolutionIndication> {
         if self.tok_is(Ident) {
             let name = self.parse_selected_name()?;
             let resolution = ResolutionIndication::Function(
@@ -1003,7 +1003,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
 
     }
 
-    pub fn parse_subtype_indication(&mut self) -> PResult<SubtypeIndication> {
+    fn parse_subtype_indication(&mut self) -> PResult<SubtypeIndication> {
         let mut subtype = SubtypeIndication::default();
         let resolution = self.parse_resolution_indication()?;
 
@@ -1068,7 +1068,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
         Ok(port)
     }
 
-    pub fn parse_interface_generic_map(&mut self) -> PResult<InterfaceGenericMap> {
+    fn parse_interface_generic_map(&mut self) -> PResult<InterfaceGenericMap> {
         debug_assert!(self.tok.kind == Generic);
 
         let start = self.pos();
@@ -1656,7 +1656,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
 
     }
 
-    pub fn parse_use_clause(&mut self) -> PResult<UseClause> {
+    fn parse_use_clause(&mut self) -> PResult<UseClause> {
         debug_assert!(self.tok.kind == Use);
         let start = self.pos();
         self.advance_tok();
@@ -1783,7 +1783,7 @@ impl<'srcfile> ParseInfo<'srcfile> {
 
     }
 
-    pub fn parse_entity_decl(&mut self) -> PResult<EntityDeclaration> {
+    fn parse_entity_decl(&mut self) -> PResult<EntityDeclaration> {
         debug_assert!(self.kind() == Entity);
         let start = self.pos();
         self.advance_tok();
@@ -1921,5 +1921,232 @@ impl<'srcfile> ParseInfo<'srcfile> {
         self.advance_tok();
 
         Ok(entity)
+    }
+}
+
+
+
+
+#[test]
+fn test_names() {
+    let names = [
+        "ieee.std_logic_1164.all",
+        "attr'high",
+        "push [ieee.std_logic return bit]",
+        "vec(5)",
+        "ifles'name(5)",
+        "ifles'name.uea(5)",
+        "push [ieee.std_logic return bit]'path",
+    ];
+
+    for &name in names.iter() {
+        println!();
+        println!("Testing Name: {}", name);
+        let mut ctx    : ParseContext = name.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_name = parser.parse_name();
+
+
+        let _ast_name = ast_name.unwrap();
+        //println!("{:#?}", ast_name);
+        println!("{:?}", parser.tok);
+        //println!("length: {:?}", name.len());
+        assert!(parser.tok.kind == TokenKind::EoF);
+    }
+}
+
+
+//"vector_arr((others => '0'), (31 downto 0 => '1'), x\"x\")",
+// Bug: There seems to be something wrong with the lexer 'x"x"' should be parsed
+// as one string literal token but it seems to become two seperate ones.
+#[test]
+fn test_exprs() {
+    let exprs = [
+        "127",
+        "(127)",
+        "5 + 5",
+        "(5 + 5)",
+        "(5 * 3 + 5 ** 2)",
+        "(a = 5 or (??b))",
+        "rising_edge(clk) and o_vld = '1'",
+        "(127 downto 96 => '1', others => '0')",
+        "info.length + (4 - info.length(1 downto 0))",
+        "vector_arr((others => '0'), (31 downto 0 => '1'), \"x\")",
+        "arr(indices range a to f)",
+    ];
+
+    for &expr in exprs.iter() {
+        println!();
+        println!("Testing Expr: {}", expr);
+
+        let mut ctx : ParseContext = expr.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_expr = parser.parse_expression();
+        //println!("Res: {:#?}", ast_expr);
+        if ast_expr.is_err() {
+            println!("Error: {:?}", ast_expr);
+        }
+        assert!(ast_expr.is_ok());
+
+        let ast_expr = ast_expr.unwrap();
+        println!("Res: {:#?}", ast_expr);
+
+        assert!(parser.tok.kind == TokenKind::EoF);
+    }
+}
+
+#[test]
+fn test_subtype_indications() {
+    let tests = [
+        "std_logic_vector",
+        "std_logic_vector(0 to 31)",
+        "std_logic_vector(data_bits - 1 downto 0)",
+        "natural range 0 to natural'high",
+        "array_sig range indices'range",
+        "arr(indices range a to g)",
+        "resolved std_ulogic",
+        "resolved std_ulogic_vector(5 downto 0)",
+        "(resolved) std_ulogic_vector",
+        "(vld ored, req anded, data xored) struct_type",
+    ];
+
+
+    for &test in tests.iter() {
+        println!();
+        println!("Testing: {}", test);
+
+        let mut ctx : ParseContext = test.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_test = parser.parse_subtype_indication();
+        if !ast_test.is_ok() {
+            println!("Err: {:?}", ast_test);
+        }
+        assert!(ast_test.is_ok());
+
+        let ast_test = ast_test.unwrap();
+        println!("Res: {:#?}", ast_test);
+
+        assert!(parser.tok.kind == TokenKind::EoF);
+    }
+}
+
+#[test]
+fn test_entity_declarations() {
+    let tests = [
+"\
+entity e is
+end entity;
+",
+"\
+entity test is
+generic(data_bits: natural := 8);
+port(i,c,b: bit; o: out bit_vector(31 downto 0));
+end entity;\
+",
+"\
+entity a is
+generic(type data_t; address: natural; procedure set_alarm(signal is_alarm: inout bit) is <>);
+end entity a;
+",
+"\
+entity weird is
+port(o: out std_logic_vector(31 downto 0) := std_logic_vector(
+        7 => 'x',
+        6 => '0',
+        5 downto 0 => 'w',
+        others => '-'));
+end entity;
+",
+
+    ];
+
+    for &test in tests.iter() {
+        println!();
+        println!("Testing: {}", test);
+
+        let mut ctx : ParseContext = test.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_test = parser.parse_entity_decl();
+        if !ast_test.is_ok() {
+            println!("Err: {:?}", ast_test);
+        }
+        assert!(ast_test.is_ok());
+
+        let ast_test = ast_test.unwrap();
+        println!("Res: {:#?}", ast_test);
+
+        assert!(parser.tok.kind == TokenKind::EoF);
+    }
+}
+
+#[test]
+fn test_generic_map() {
+    let tests = [
+        "generic map (complex_fixed_left =>
+complex_math_fixed_left,
+complex_fixed_right =>
+complex_math_fixed_right,
+complex_fixed_formal_pkg =>
+complex_math_fixed_formal_pkg)
+",
+    ];
+
+
+    for &test in tests.iter() {
+        println!();
+        println!("Testing: {}", test);
+
+        let mut ctx : ParseContext = test.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_test = parser.parse_interface_generic_map();
+        if !ast_test.is_ok() {
+            println!("Err: {:?}", ast_test);
+        }
+        assert!(ast_test.is_ok());
+
+        let ast_test = ast_test.unwrap();
+        println!("Res: {:#?}", ast_test);
+
+        assert!(parser.tok.kind == TokenKind::EoF);
+    }
+}
+
+#[test]
+fn test_type_decl() {
+    let tests = [
+        // Integer point types
+        "type A is range 1 to 10;",
+        "type MULTI_LEVEL_LOGIC is (LOW, HIGH, RISING, FALLING, AMBIGUOUS);",
+        "type BIT is ('0','1');",
+        "type SWITCH_LEVEL is ('0','1','X');",
+"type DURATION is range -1E18 to 1E18
+units
+    fs; --femtosecond
+    ps = 1000 fs; --picosecond
+    ns = 1000 ps; --nanosecond
+    us = 1000 ns; --microsecond
+    ms = 1000 us; --millisecond
+    sec = 1000 ms; --second
+    min = 60 sec; --minute
+end units;",
+    ];
+
+
+    for &test in tests.iter() {
+        println!();
+        println!("Testing: {}", test);
+
+        let mut ctx : ParseContext = test.into();
+        let mut parser : ParseInfo = (&mut ctx).into();
+        let ast_test = parser.parse_type_decl();
+        if !ast_test.is_ok() {
+            println!("Err: {:?}", ast_test);
+        }
+        assert!(ast_test.is_ok());
+
+        let ast_test = ast_test.unwrap();
+        println!("Res: {:#?}", ast_test);
+
+        assert!(parser.tok.kind == TokenKind::EoF);
     }
 }
